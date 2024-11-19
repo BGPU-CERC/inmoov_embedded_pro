858227bca74e959809bb01cd436ccb27cb97caeb
SnJon <snjon93@gmail.com>
2024-11-19 22:43:02 +0300

    update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,11 +1,32 @@
 #include "src/modbus/modbus.h"
+#include "src/command/command.h"
+#include "src/servo/servo.h"
+
+TLV *packet = getModbusTlv();
 
 void setup()
 {
-
+    Serial.begin(115200);
+    Serial3.begin(115200);
+    setupServo();
 }
 
 void loop()
 {
+    tickServo();
+}
 
+void serialEvent()
+{
+    while (Serial.available())
+    {
+        unsigned char c = Serial.read();
+        readModbus(c);
+
+        if (isModbusComplete())
+        {
+            processCommand(packet);
+            resetModbus();
+        }
+    }
 }
